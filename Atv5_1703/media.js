5e5a09b6e218325eea8d943d05181b08907151cf
Blackelcio <39889080+Blackelcio@users.noreply.github.com>
2021-03-17 22:51:45 -0300

    Add files via upload

---
+++ Atv5_1703/media.js
@@ -0,0 +1,18 @@
+var aluno;
+var nota1 = 0;
+var nota2 = 0;
+var nota3 = 0;
+var media = 0;
+
+aluno = prompt("Digite o seu nome: ", "");
+
+nota1 = prompt("Digite a sua nota 1","Digite aqui!");
+nota2 = prompt("Digite a sua nota 2","Digite aqui!");
+nota3 = prompt("Digite a sua nota 3","Digite aqui!");
+
+media = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3))/3;
+
+document.write("Média do aluno " + aluno);
+
+document.write("<br>Sua média é " + media);
+
